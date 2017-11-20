586775698166ee9aeda06d9f6276863119dbe56d
Roberto_Jr <robertomartins.m.jr@gmail.com>
2017-11-19 22:18:24 -0200

    Criando Dominio

---
+++ Dominio/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nome { get; set; }
+        public DateTime dtNascimento { get; set; }
+        public string regiao { get; set; }
+        public int qtdPontos { get; set; }
+        public int qtdOuro { get; set; }
+        public int qtdPrata { get; set; }
+        public int qtdBronze { get; set; }
+
+        public Cliente() { }
+
+        public Cliente(string id, string nome, DateTime dtNascimento, string regiao, int qtdPontos)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.dtNascimento = dtNascimento;
+            this.regiao = regiao;
+            this.qtdPontos = qtdPontos;
+            this.qtdOuro = qtdPontos / 10000;
+            this.qtdPrata = (qtdPontos % 10000) / 1000;
+            this.qtdBronze = (qtdPontos % 1000) / 100;
+        }
+
+        public int GetIdade()
+        {
+            var dtAgora = DateTime.Now;
+            var idade = dtAgora.Year - dtNascimento.Year;
+
+            if (dtAgora.Month < dtNascimento.Month || (dtAgora.Month == dtNascimento.Month && dtAgora.Day < dtNascimento.Day))
+                idade--;
+
+            return idade;
+        }
+    }
+}
--- SQTEC_Fidelidade/Program.cs
+++ SQTEC_Fidelidade/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Dominio;
 
 namespace SQTEC_Fidelidade
 {
@@ -10,7 +13,73 @@ namespace SQTEC_Fidelidade
     {
         static void Main(string[] args)
         {
-            Console.Write("Olá mundo!!!");
+            String linha;
+
+            try
+            {
+                //Cria um novo StreamReader passando o nome do arquivo que será lido. 
+                //Como o arquivo vai estar no mesmo diretório que o .exe, não é necessário passar o caminho.
+                StreamReader sr = new StreamReader("clientes.txt");
+                List<Cliente> listaClientes = new List<Cliente>();
+
+                //Lendo a primeira linha do arquivo
+                linha = sr.ReadLine();
+
+                //Caso o valor lido não seje nulo, continua lendo as demais linha até o fim do arquivo.
+                while (linha != null)
+                {
+                    var linhaSplit = linha.Split(';');
+                    listaClientes.Add(new Cliente(linhaSplit[0], linhaSplit[1], DateTime.Parse(linhaSplit[2]), linhaSplit[3], int.Parse(linhaSplit[4])));
+
+                    //Read the next line
+                    linha = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+
+                foreach (var cliente in listaClientes.OrderBy(c => c.nome))
+                {
+                    Console.WriteLine((cliente.nome + " - " + cliente.regiao).PadRight(35, ' ') + " | " + cliente.GetIdade().ToString().PadLeft(3, ' ') + " anos | Ouro: " + cliente.qtdOuro.ToString().PadLeft(4, ' ') + " | Prata: " + cliente.qtdPrata.ToString().PadLeft(2, ' ') + " | Bronze: " + cliente.qtdBronze.ToString().PadLeft(2, ' '));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                List<Cliente> result = listaClientes
+                    .GroupBy(c => c.regiao)
+                    .Select(cl => new Cliente
+                    {
+                        regiao = cl.First().regiao,
+                        qtdOuro = cl.Sum(c => c.qtdOuro),
+                        qtdPrata = cl.Sum(c => c.qtdPrata),
+                        qtdBronze = cl.Sum(c => c.qtdBronze),
+                    }).ToList();
+
+                foreach (var cliente in result.OrderBy(c => c.regiao))
+                {
+                    Console.WriteLine(cliente.regiao.PadRight(20, ' ') + " | Ouro: " + cliente.qtdOuro.ToString().PadLeft(4, ' ') + " | Prata: " + cliente.qtdPrata.ToString().PadLeft(2, ' ') + " | Bronze: " + cliente.qtdBronze.ToString().PadLeft(2, ' '));
+                }
+
+                //open file stream
+                using (StreamWriter file = File.CreateText("data.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+
+                    //serialize object directly into file stream
+                    serializer.Serialize(file, listaClientes);
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
