4fa3d9040c6bb214eb7516a181ed512a333e9a0a
Roberto_Jr <robertomartins.m.jr@gmail.com>
2017-11-19 23:41:02 -0200

    Finalizado

--- SQTEC_Fidelidade/Program.cs
+++ SQTEC_Fidelidade/Program.cs
@@ -6,44 +6,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Dominio;
+using Servico;
 
 namespace SQTEC_Fidelidade
 {
     class Program
     {
+        internal static readonly ClienteService _clienteService = new ClienteService();
+
         static void Main(string[] args)
         {
-            String linha;
-
             try
             {
-                //Cria um novo StreamReader passando o nome do arquivo que será lido. 
-                //Como o arquivo vai estar no mesmo diretório que o .exe, não é necessário passar o caminho.
-                StreamReader sr = new StreamReader("clientes.txt");
-                List<Cliente> listaClientes = new List<Cliente>();
-
-                //Lendo a primeira linha do arquivo
-                linha = sr.ReadLine();
-
-                //Caso o valor lido não seje nulo, continua lendo as demais linha até o fim do arquivo.
-                while (linha != null)
-                {
-                    var linhaSplit = linha.Split(';');
-                    listaClientes.Add(new Cliente(linhaSplit[0], linhaSplit[1], DateTime.Parse(linhaSplit[2]), linhaSplit[3], int.Parse(linhaSplit[4])));
-
-                    //Read the next line
-                    linha = sr.ReadLine();
-                }
-
-                //close the file
-                sr.Close();
+                var listaClientes = _clienteService.AtualizaDados(_clienteService.LerArquivo());
 
                 foreach (var cliente in listaClientes.OrderBy(c => c.nome))
                 {
                     Console.WriteLine((cliente.nome + " - " + cliente.regiao).PadRight(35, ' ') + " | " + cliente.GetIdade().ToString().PadLeft(3, ' ') + " anos | Ouro: " + cliente.qtdOuro.ToString().PadLeft(4, ' ') + " | Prata: " + cliente.qtdPrata.ToString().PadLeft(2, ' ') + " | Bronze: " + cliente.qtdBronze.ToString().PadLeft(2, ' '));
                 }
 
-                Console.WriteLine();
                 Console.WriteLine();
 
                 List<Cliente> result = listaClientes
@@ -61,24 +42,11 @@ namespace SQTEC_Fidelidade
                     Console.WriteLine(cliente.regiao.PadRight(20, ' ') + " | Ouro: " + cliente.qtdOuro.ToString().PadLeft(4, ' ') + " | Prata: " + cliente.qtdPrata.ToString().PadLeft(2, ' ') + " | Bronze: " + cliente.qtdBronze.ToString().PadLeft(2, ' '));
                 }
 
-                //open file stream
-                using (StreamWriter file = File.CreateText("data.json"))
-                {
-                    JsonSerializer serializer = new JsonSerializer();
-
-                    //serialize object directly into file stream
-                    serializer.Serialize(file, listaClientes);
-                }
-
                 Console.ReadLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
             }
         }
     }
---
+++ Servico/ClienteService.cs
@@ -0,0 +1,99 @@
+﻿using Dominio;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico
+{
+    public class ClienteService
+    {
+        public List<Cliente> LerArquivo()
+        {
+            string linha;
+            //Cria um novo StreamReader passando o nome do arquivo que será lido. 
+            //Como o arquivo vai estar no mesmo diretório que o .exe, não é necessário passar o caminho.
+            StreamReader sr = new StreamReader("clientes.txt");
+            List<Cliente> listaClientes = new List<Cliente>();
+
+            //Lendo a primeira linha do arquivo
+            linha = sr.ReadLine();
+
+            //Caso o valor lido não seje nulo, continua lendo as demais linha até o fim do arquivo.
+            while (linha != null)
+            {
+                var linhaSplit = linha.Split(';');
+                listaClientes.Add(new Cliente(linhaSplit[0], linhaSplit[1], DateTime.Parse(linhaSplit[2]), linhaSplit[3], int.Parse(linhaSplit[4])));
+
+                //Read the next line
+                linha = sr.ReadLine();
+            }
+
+            //close the file
+            sr.Close();
+
+            return listaClientes;
+        }
+
+        public List<Cliente> LerDados()
+        {
+            string linha;
+            List<Cliente> listaClientes = new List<Cliente>();
+
+            //Cria um novo StreamReader passando o nome do arquivo que será lido. 
+            //Como o arquivo vai estar no mesmo diretório que o .exe, não é necessário passar o caminho.
+            StreamReader sr = new StreamReader("data.json");
+
+            //Lendo a primeira linha do arquivo
+            linha = sr.ReadLine();
+
+            //Caso o valor lido não seje nulo, continua lendo as demais linha até o fim do arquivo.
+            if (linha != null)
+            {
+                listaClientes = JsonConvert.DeserializeObject<List<Cliente>>(linha);
+            }
+
+            //fecha o arquivo
+            sr.Close();
+
+            return listaClientes;
+        }
+
+        public void SalvarDados(List<Cliente> listaClientes)
+        {
+            //open file stream
+            using (StreamWriter file = File.CreateText("data.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+
+                //serialize object directly into file stream
+                serializer.Serialize(file, listaClientes);
+            }
+        }
+
+        public List<Cliente> AtualizaDados(List<Cliente> lstClientesArquivo)
+        {
+            var lstClientesDados = LerDados();
+
+            foreach (var cliente in lstClientesArquivo)
+            {
+                var clienteDados = lstClientesDados.Where(c => c.id == cliente.id).FirstOrDefault();
+                if (clienteDados != null)
+                {
+                    lstClientesDados[lstClientesDados.IndexOf(clienteDados)] = cliente;
+                }
+                else
+                {
+                    lstClientesDados.Add(cliente);
+                }
+            }
+
+            SalvarDados(lstClientesDados);
+
+            return lstClientesDados;
+        }
+    }
+}
